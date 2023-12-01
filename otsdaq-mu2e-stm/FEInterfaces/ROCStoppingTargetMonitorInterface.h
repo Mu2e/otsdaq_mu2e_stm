445bc62a314505846be770e56610224b156c5be7
Eric Flumerfelt <eflumerf@fnal.gov>
2023-12-01 09:10:49 -0600

    Add new methods from ROCCoreVInterface

--- otsdaq-mu2e-stm/FEInterfaces/ROCStoppingTargetMonitorInterface.h
+++ otsdaq-mu2e-stm/FEInterfaces/ROCStoppingTargetMonitorInterface.h
@@ -46,6 +46,8 @@ public:
 
   	virtual int  							readDTCLinkLossCounter	(void) override;
   	virtual void 							resetDTCLinkLossCounter	(void) override;
+	virtual void							GetStatus				(void) override { return; }
+	virtual void							GetFirmwareVersion		(void) override { return; }
 
   
 
